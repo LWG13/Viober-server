aa444041d50c5403b388001579e37ebd03b0d69a
LWG13 <164619164+LWG13@users.noreply.github.com>
2025-04-13 17:07:18 +0700

    Update socket.cjs

--- socket.cjs
+++ socket.cjs
@@ -2,15 +2,13 @@ const { Server } = require('socket.io');
 const express = require("express")
 const app = express();
 
-const httpServer = app.listen(3000, () => {
-  console.log(`API server listening at 3000`);
-});
+const server = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
     origin: "https://a7967ab8-1bb8-4301-b7e9-ac474e9dda2a-00-16da4blhmya43.pike.replit.dev",
     methods: ["GET", "POST", "PUT"],
-    transports: ["polling"],
+    transports: ["websocket"],
   }
 });
 io.on('connection', socket => {
