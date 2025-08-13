4502badd111bc46c29675b3a02568af627bfd614
LWG <164619164+LWG13@users.noreply.github.com>
2025-08-13 15:55:13 +0700

    Update chatModel.cjs

--- chatModel.cjs
+++ chatModel.cjs
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const chatSchema = new mongoose.Schema({
     user1: String,
     user2: String,
-    image1: String,
+    image1: String, 
     username1: String,
     image2: String,
     username2: String,
